e5a59768f93062c5a30c7e7e19efdc6cf880cbcf
Dandi Ahmadin <ahmadin.dandi14@gmail.com>
2020-11-09 21:22:32 +0700

    Initial Commit

---
+++ src/id/dandiahmadin/tabungan/Main.java
@@ -0,0 +1,33 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package id.dandiahmadin.tabungan;
+import java.util.Scanner;
+
+/**
+ *
+ * @author
+ * NAMA     : Dandi Ahmadin
+ * KELAS    : IF-1
+ * NIM      : 10119033
+ * Deskripsi Program : Program ini berisi Tabungan dengan pendekatan object oriented
+ */
+public class Main {
+    private static int saldo, jumlah;
+    
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+        
+        System.out.println("====Program Penarikan Uang====");
+        System.out.print("Masukan Saldo Awal : ");
+        saldo = input.nextInt();
+        
+        System.out.print("Jumlah uang yang diambil : ");
+        jumlah = input.nextInt();
+        
+        Tabungan tabungan1 = new Tabungan(saldo);
+        System.out.println("Saldo Anda Sekarang : " + tabungan1.ambilUang(jumlah));
+    }
+}
---
+++ src/id/dandiahmadin/tabungan/Tabungan.java
@@ -0,0 +1,26 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package id.dandiahmadin.tabungan;
+
+/**
+ *
+ * @author
+ * NAMA     : Dandi Ahmadin
+ * KELAS    : IF-1
+ * NIM      : 10119033
+ * Deskripsi Program : Program ini berisi class Tabungan
+ */
+public class Tabungan {
+    private int saldo;
+    
+    public Tabungan(int saldo) {
+        this.saldo = saldo;
+    }
+    
+    public int ambilUang(int jumlah) {
+        return this.saldo - jumlah; 
+    }
+}
